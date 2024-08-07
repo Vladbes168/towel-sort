6b25aeec3b9261ad2f6acdc04f7e026d79f41e7f
Vlad Bezsonov <vd115313@gmail.com>
2024-08-07 18:35:10 +0200

    feat: resolved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-
+const arr = [
+  [ 1, 2, 3 ],
+  [ 4, 5, 6 ],
+  [ 7, 8, 9 ],
+  [ 10, 11, 12 ],
+ ]
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix === undefined) return []
+  let result = []
+  for(let i = 0; i < matrix.length ; i++) {
+    if(i % 2 === 0) {
+     result = result.concat(matrix[i]);
+    } else {
+      result = result.concat(matrix[i].reverse());
+    }
+    
+  }
+  return result;
 }
+// console.log(towelSort(arr));
+// console.log(arr.length);
+
+
